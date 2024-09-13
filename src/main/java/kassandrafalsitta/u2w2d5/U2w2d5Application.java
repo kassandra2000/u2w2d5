f024870a5935ab12d83eae0bde63bc1b33bdb4df
kassandra falsitta <160047671+kassandra2000@users.noreply.github.com>
2024-09-13 09:30:38 +0200

    create project

---
+++ src/main/java/kassandrafalsitta/u2w2d5/U2w2d5Application.java
@@ -0,0 +1,13 @@
+package kassandrafalsitta.u2w2d5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w2d5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w2d5Application.class, args);
+	}
+
+}
